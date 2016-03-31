4d798c9250db236333e8ac15e6605a79915d4bbe
Kyle Kwon <kyleskwon@gmail.com>
2016-03-31 14:51:56 -0700

    refactor animatePoints

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,14 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
- 
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
- 
-    for(var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    $.each($('.point'), revealPoint);
 };
 
-$(window).load = (function() {
+$(window).load(function() {
     if ($(window).height() > 950) {
         animatePoints();
     }
@@ -22,8 +16,8 @@ $(window).load = (function() {
     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
     
     $(window).scroll(function(event) {
-        if ($(window).scrollTop() >= scrollDistance){
-            animatePoints();    
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
 });
