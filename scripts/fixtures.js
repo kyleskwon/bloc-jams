431a845a0d8659e2c2ac2143107359124e4791a1
Kyle Kwon <kyleskwon@gmail.com>
2016-04-03 10:24:31 -0700

    complete fixtures.js

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -12,3 +12,18 @@ var albumPicasso = {
         { title: 'Magenta', duration: '2:15' }
     ]
 };
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png)',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' }
+        { title: 'Ring, ring, ring', duration: '5:01' }
+        { title: 'Fits in your pocket', duration: '3:21' }
+        { title: 'Can you hear me now?', duration: '3:14' }
+        { title: 'Wrong phone number', duration: '2:15' }
+    ]
+};
