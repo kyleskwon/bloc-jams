e5ed01c436479b281c42af8118071b473c5b8832
Kyle Kwon <kyleskwon@gmail.com>
2016-04-03 10:21:12 -0700

    create fixtures.js

---
+++ scripts/fixtures.js
@@ -0,0 +1,14 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pable Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png)',
+    songs: [
+        { title: 'Blue', duration: '4:26' }
+        { title: 'Green', duration: '3:14' }
+        { title: 'Red', duration: '5:01' }
+        { title: 'Pink', duration: '3:21' }
+        { title: 'Magenta', duration: '2:15' }
+    ]
+};
