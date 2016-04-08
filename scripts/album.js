d7a418d8d0e8ff5fc6dffe31c69a58ab6c93e38c
Kyle Kwon <kyleskwon@gmail.com>
2016-04-08 10:56:25 -0700

    double check for player bar play button functionality

--- scripts/album.js
+++ scripts/album.js
@@ -1,46 +1,15 @@
- var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21' },
-         { title: 'Magenta', duration: '2:15' }
-     ]
- };
- 
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21' },
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15' }
-     ]
- };
-
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
     '<tr class="album-view-song-item">'
-    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-    + '  <td class="song-item-title">' + songName + '</td>'
-    + '  <td class="song-item-duration">' + songLength + '</td>'
-    + '</tr>'
-    ;
- 
+  + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+  + '  <td class="song-item-title">' + songName + '</td>'
+  + '  <td class="song-item-duration">' + songLength + '</td>'
+  + '</tr>'
+  ;
+    
     var $row = $(template);
     
     var clickHandler = function() {
-        
         var songNumber = parseInt($(this).attr('data-song-number'));
         
         if (currentlyPlayingSongNumber !== null) {
@@ -98,7 +67,7 @@ var setCurrentAlbum = function(album) {
     $albumTitle.text(album.title);
     $albumArtist.text(album.artist);
     $albumReleaseInfo.text(album.year + ' ' + album.label);
-    $albumImage.text('src', album.albumArtUrl);
+    $albumImage.attr('src', album.albumArtUrl);
  
     $albumSongList.empty();
  
--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -5,10 +5,10 @@ var albumPicasso = {
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png)',
     songs: [
-        { title: 'Blue', duration: '4:26' }
-        { title: 'Green', duration: '3:14' }
-        { title: 'Red', duration: '5:01' }
-        { title: 'Pink', duration: '3:21' }
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21' },
         { title: 'Magenta', duration: '2:15' }
     ]
 };
@@ -20,10 +20,10 @@ var albumMarconi = {
     year: '1909',
     albumArtUrl: 'assets/images/album_covers/20.png)',
     songs: [
-        { title: 'Hello, Operator?', duration: '1:01' }
-        { title: 'Ring, ring, ring', duration: '5:01' }
-        { title: 'Fits in your pocket', duration: '3:21' }
-        { title: 'Can you hear me now?', duration: '3:14' }
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now?', duration: '3:14' },
         { title: 'Wrong phone number', duration: '2:15' }
     ]
 };
